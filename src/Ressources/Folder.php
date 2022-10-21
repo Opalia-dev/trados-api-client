a51bf402bcf420035bb26d96a7f3bf5042b38b86
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:34:17 +0200

    ADD folder endpoints

---
+++ src/Ressources/Folder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class Folder{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function get(string $folderId)
+    {
+        return $this->client->get('/folders/'.$folderId);
+    }
+
+    public function getRoot()
+    {
+        return $this->client->get('/folders/root');
+    }
+
+    public function list()
+    {
+        return $this->client->get('/folders');
+    }
+}
