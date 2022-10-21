1fa42a67ba69a509f0c7d4398ea00ef610b1fe04
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:54:21 +0200

    ADD language endpoints

---
+++ src/Ressources/Language.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class Language{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function list()
+    {
+        return $this->client->get('/languages');
+    }
+}
