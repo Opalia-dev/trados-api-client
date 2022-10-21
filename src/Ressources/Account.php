b774d7a23944ebd99433d23b6c81e06e7fbb4c25
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:33:50 +0200

    ADD account endpoints

---
+++ src/Ressources/Account.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class Account{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function list()
+    {
+        return $this->client->get('/accounts');
+    }
+}
