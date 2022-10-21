913263cf8cfbe7519f2d4dd81e5352b54374ed75
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:55:07 +0200

    ADD projectTemplate endpoints

---
+++ src/Ressources/ProjectTemplate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class ProjectTemplate{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function get(string $projectTemplateId)
+    {
+        return $this->client->get('/project-templates/'.$projectTemplateId);
+    }
+
+    public function list()
+    {
+        return $this->client->get('/project-templates');
+    }
+}
