2d694655bf6c7226b19d78fe3682d8964dbd5ed4
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:54:10 +0200

    ADD group endpoints

---
+++ src/Ressources/Group.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class Group{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function get(string $groupId)
+    {
+        return $this->client->get('/groups/'.$groupId);
+    }
+
+    public function list()
+    {
+        return $this->client->get('/groups');
+    }
+}
