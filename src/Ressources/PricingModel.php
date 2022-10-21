c1b5cf557bed7b6ebad7a46ce9c9f200e11a0d64
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:54:45 +0200

    ADD pricingModel endpoints

---
+++ src/Ressources/PricingModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class PricingModel{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function get(string $pricingModelId)
+    {
+        return $this->client->get('/pricing-models/'.$pricingModelId);
+    }
+
+    public function list()
+    {
+        return $this->client->get('/pricing-models');
+    }
+}
