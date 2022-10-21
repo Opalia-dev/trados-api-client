bf23ec4196b27d1039f88476a9339155e7999ff9
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:34:25 +0200

    ADD file endpoints

---
+++ src/Ressources/File.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class File{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function pollUploadZip(string $fileId)
+    {
+        return $this->client->get('/files/'.$fileId);
+    }
+
+    public function uploadZip(string $file)
+    {
+        return $this->client->post('/Files',['json'=>['file'=>$file]]);
+    }
+}
