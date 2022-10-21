68406cf19d801d35a5f222603122fdb17a6c4929
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:54:35 +0200

    ADD languageProcessing endpoints

---
+++ src/Ressources/LanguageProcessing.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class LanguageProcessing{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function getRules(string $languageProcessingRuleId)
+    {
+        return $this->client->get('/languageProcessingRules/'.$languageProcessingRuleId);
+    }
+
+    public function listRules()
+    {
+        return $this->client->get('/languageProcessingRules');
+    }
+}
