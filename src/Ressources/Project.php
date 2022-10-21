25e37b37e1467b6ee01d786fb3ddb4e5291fb931
Squareinnov <contact@squareinnov.fr>
2022-10-21 14:44:52 +0200

    ADD projets endpoints

--- src/Ressources/Project.php
+++ src/Ressources/Project.php
@@ -46,8 +46,56 @@ Class Project{
         return $this->client->put('/projects/'.$projectId.'/files/'.$fileId.'/cancel');
     }
 
-    // public function create(ProjectModel $projectModel,string $fields = null)
-    // {
+    public function create(array $body,string $fields = null)
+    {
+        $params['json']= $body;
 
-    // }
+        if($fields !==null){
+            $params['query'] = [
+                'fields' => $fields
+            ];
+        }
+        return $this->client->post('/projects/create',$params);
+    }
+
+    public function getConfig(int $projectId, string $fields = null)
+    {
+        if($fields !==null){
+            return $this->client->get('/projects/'.$projectId,[
+                'query' => [
+                    'fields' => $fields
+                ]
+            ]);
+        }
+        return $this->client->get('/projects/'.$projectId.'/configuration');
+    }
+
+    public function updateConfig(int $projectId, array $body)
+    {
+        return $this->client->get('/projects/'.$projectId.'/configuration',['json'=>$body]);
+    }
+
+    public function getTasks(int $projectId, string $fields = null,array $location = null, string $locationStrategy = null,int $skip = null,string $sort = null, int $top = null)
+    {
+        $params = [];
+        if($fields !==null){
+            $params['query']['fields'] = $fields;
+        }
+        if($location!==null){
+            $params['query']['location']=$location;
+        }
+        if($locationStrategy!==null){
+            $params['query']['locationStrategy']=$locationStrategy;
+        }
+        if($skip!==null){
+            $params['query']['skip']=$skip;
+        }
+        if($sort!==null){
+            $params['query']['sort']=$sort;
+        }
+        if($top!==null){
+            $params['query']['top']=$top;
+        }
+        return $this->client->get('/projects/'.$projectId.'/tasks',$params);
+    }
 }
