fed3ee49cf6bbd7c4a67b66b7893b3ea65ca3c8c
Squareinnov <contact@squareinnov.fr>
2022-10-21 15:34:35 +0200

    ADD customField endpoints

---
+++ src/Ressources/CustomField.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Opalia\TradosApiClient\Ressources;
+
+use Opalia\TradosApiClient\Trados;
+
+Class CustomField{
+
+    public function __construct(private Trados $client)
+    {}
+
+    public function listDefinitions()
+    {
+        return $this->client->get('/custom-field-definitions');
+    }
+
+    public function getDefinition(string $customFieldDefinitionId)
+    {
+        return $this->client->get('/custom-field-definitions/'.$customFieldDefinitionId);
+    }
+}
