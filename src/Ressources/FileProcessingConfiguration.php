0c87e73e84e1cf37590e65c7bc870e823a0ebc8d
Squareinnov <contact@squareinnov.fr>
2022-10-24 09:57:44 +0200

    FIX endpoints error "processing" not "processiong"

--- src/Ressources/FileProcessingConfiguration.php
+++ src/Ressources/FileProcessingConfiguration.php
@@ -11,21 +11,21 @@ Class FileProcessingConfiguration{
 
     public function list()
     {
-        return $this->client->get('/file-processiong-configurations');
+        return $this->client->get('/file-processing-configurations');
     }
 
     public function get(string $fileProcessingConfigurationId)
     {
-        return $this->client->get('/file-processiong-configurations/'.$fileProcessingConfigurationId);
+        return $this->client->get('/file-processing-configurations/'.$fileProcessingConfigurationId);
     }
 
     public function listFileTypeSettings()
     {
-        return $this->client->get('/file-processiong-configurations/file-type-settings');
+        return $this->client->get('/file-processing-configurations/file-type-settings');
     }
 
     public function getFileTypeSettings(string $fileProcessingConfigurationId,string $fileTypeSettingId)
     {
-        return $this->client->get('/file-processiong-configurations/'.$fileProcessingConfigurationId.'/file-type-settings/'.$fileTypeSettingId);
+        return $this->client->get('/file-processing-configurations/'.$fileProcessingConfigurationId.'/file-type-settings/'.$fileTypeSettingId);
     }
 }
