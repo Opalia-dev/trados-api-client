21ff0c7377f68466c60140c13a15182cba9e5028
Squareinnov <contact@squareinnov.fr>
2022-11-10 08:44:37 +0100

    ADD workflow update and list functions

--- src/Ressources/Workflow.php
+++ src/Ressources/Workflow.php
@@ -17,4 +17,43 @@ Class Workflow{
         }
         return $this->client->get('workflows/'.$workflowId,$params);
     }
+
+    public function list(string $fields = null,array $location = null, string $locationStrategy = null,int $skip = null,string $sort = null, int $top = null)
+    {
+        $params = [];
+        if($fields !==null){
+            $params['query']['fields'] = $fields;
+        }
+        if($skip!==null){
+            $params['query']['skip']=$skip;
+        }
+        if($location!==null){
+            $params['query']['location']=$location;
+        }
+        if($locationStrategy!==null){
+            $params['query']['locationStrategy']=$locationStrategy;
+        }
+        if($sort!==null){
+            $params['query']['sort']=$sort;
+        }
+        if($top!==null){
+            $params['query']['top']=$top;
+        }
+        return $this->client->get('workflows',$params);
+    }
+
+    public function update(string $workflowId,?string $name = null,?string $description = null,?array $taskConfigurations = null)
+    {
+        $params = [];
+        if($name !==null){
+            $params['query']['name'] = $name;
+        }
+        if($description !==null){
+            $params['query']['de$description'] = $description;
+        }
+        if($taskConfigurations !==null){
+            $params['query']['taskConfigurations'] = $taskConfigurations;
+        }
+        return $this->client->get('workflows/'.$workflowId,$params);
+    }
 }
